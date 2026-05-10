3caa6375943af35436a181e14c1f90a894dbe38c
NQB37 <nqb37.dev@gmail.com>
2026-05-10 20:43:49 +0200

    feat(user-srv): add error handle middleware

---
+++ backend/user-service/src/middlewares/errorHandle.middleware.ts
@@ -0,0 +1,17 @@
+import type { ErrorRequestHandler } from 'express';
+import { AppError } from '../utils/app-error.js';
+
+export const errorHandler: ErrorRequestHandler = (error, _req, res, _next) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      message: error.message,
+      details: error.details,
+    });
+  }
+
+  console.error(error);
+
+  return res.status(500).json({
+    message: 'Internal server error',
+  });
+};
