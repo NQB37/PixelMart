e0ab0938677764ca2a999aee57898b6e54ad408c
Quoc Bao Nguyen <nqb37.dev@gmail.com>
2026-06-15 11:58:55 +0200

    feat(fe): init Frontend project

---
+++ client/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+
+export const metadata: Metadata = {
+  title: "Pixel Mart",
+  description: "Tech E-Commerce",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en' className={cn("dark", "h-full", "antialiased", "font-sans", geist.variable)}>
+      <body className='min-h-full flex flex-col'>{children}</body>
+    </html>
+  );
+}
